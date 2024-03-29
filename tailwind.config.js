188990982239e5ecc1f050edb894dae5c36c4f78
YoshimiTakuto <takuto.yoshimi.ncdc.co.jp>
2024-03-29 12:16:52 +0900

    feat: tailwindcss適用

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {},
+    backgroundImage: {
+      "tutorial-bg": "url('../../src/img/main-bg.jpg')",
+    },
   },
   plugins: [],
 };
